db2e73b24b715bddedffdb1b451e54989068c0f2
kevinRL02 <kevinleonardolopez@gmail.com>
2022-05-10 14:43:25 -0500

    Clases en los paquetes View & Controller

---
+++ UniBNB/src/Controller/RegistroController.java
@@ -0,0 +1,20 @@
+package com.example.pontimotel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegistroController {
+    public void SingUP(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Inicio.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
