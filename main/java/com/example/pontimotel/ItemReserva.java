5fc41be8911dbb2e10016be0d2a569fcf21560e6
kevinRL02 <kevinleonardolopez@gmail.com>
2022-05-11 19:29:33 -0500

    Intellij

--- main/java/com/example/pontimotel/ItemReserva.java
+++
@@ -1,8 +0,0 @@
-package com.example.pontimotel;
-
-import javafx.scene.control.Label;
-
-public class ItemReserva {
-    public Label Label1;
-}
-
--- main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pontimotel {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.pontimotel to javafx.fxml;
-    exports com.example.pontimotel;
-}
